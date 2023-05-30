acda83aeacdfca4d5020a9fda3c012533144e309
Pranav Mendiratta <pranavmendi@gmail.com>
2023-05-30 17:03:55 +0530

    Initial commit

---
+++ src/test/java/com/example/devopsassignment/PranavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PranavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
